9ee9edb1bbcfa32e09d82d39d9885c5ba629b6c8
Akbar Gherbal <akbar.gherbal@gmail.com>
2025-01-14 08:24:26 +0300

    Create superuser

--- my_project/diacriticizer/urls.py
+++ my_project/diacriticizer/urls.py
@@ -5,7 +5,9 @@ app_name = "diacriticizer"
 
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path(
+        "", views.home, name="home"
+    ),  # this makes the homepage reachable by `/` route.
     path("training/", views.training, name="training"),
     path("index/", views.index, name="index"),
 ]
--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("diacriticizer.urls")),
+    path("", include("diacriticizer.urls")),  # This line is essential!
     path("diacriticizer/", include("diacriticizer.urls")),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
